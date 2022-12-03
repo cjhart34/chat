c2e84278687bce97f2dad6a9a00a79b967732daf
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-12-03 08:23:37 -0600

    initial commit

---
+++ Chat/Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Corey Harthoorn on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
